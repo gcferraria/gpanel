bd9beac2faaeafd31790ad527e1c979c7a706b3d
Gonçalo Ferraria <gferraria@gmail.com>
2018-09-08 17:31:36 +0100

    Increase session timeout

--- applications/admin/config/config.php
+++ applications/admin/config/config.php
@@ -392,7 +392,7 @@ $config['sess_cookie_name'] = 'gpanel_session';
 $config['sess_expiration'] = 7200;
 $config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = TRUE;
-$config['sess_time_to_update'] = 300;
+$config['sess_time_to_update'] = 7200;
 $config['sess_regenerate_destroy'] = FALSE;
 
 /*
