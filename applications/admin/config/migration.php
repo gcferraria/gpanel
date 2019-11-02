857ab92cd45b2b5417cb0005711c725b7184885d
Gonçalo Ferraria <gferraria@gmail.com>
2019-11-02 11:34:40 +0000

    Move sql to common

--- applications/admin/config/migration.php
+++ applications/admin/config/migration.php
@@ -81,4 +81,4 @@ $config['migration_version'] = 20191013165400;
 | Also, writing permission is required within the migrations path.
 |
 */
-$config['migration_path'] = APPPATH.'migrations/';
+$config['migration_path'] = HOMEPATH . 'applications/common/migrations/';
