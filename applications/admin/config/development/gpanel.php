4fdb7b029f4cb112894760185188760033cb5dd2
Gonçalo Ferraria <gferraria@gmail.com>
2020-05-11 23:08:02 +0100

    Added gpanel config for development enviroment

---
+++ applications/admin/config/development/gpanel.php
@@ -0,0 +1,35 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * --------------------------------
+ * Config: Static URL
+ * --------------------------------
+ * Default value:
+ * $config['static_url'] = "";
+ *
+ * Url used for access to static resources.
+ */
+$config['static_url'] = '';
+
+/**
+ * --------------------------------
+ * Config: Online Services Email
+ * --------------------------------
+ * Default value:
+ * $config['noreply_email'] = "";
+ *
+ * Email for send Notifications in Online Services.
+ */
+ $config['noreply_email'] = 'noreply@domain.pt';
+
+/**
+ * --------------------------------
+ * Config: Online Services Name
+ * --------------------------------
+ * Default value:
+ * $config['noreply_name'] = "";
+ *
+ * Name for send Notifications in Online Services.
+ */
+ $config['noreply_name'] = 'Online Services';
